d21725e0f86f71a380b5f572abb7a88be35e5d78
GergelyV <gergelyv@topdesk.com>
2021-06-28 09:00:39 +0200

    Menu block added

---
+++ src/components/wa-menu-block.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="button is-success is-menu-card-size is-rounded-small is-flex is-flex-direction-column is-justify-content-space-around">
+		<fa-icon
+			class="mt-3"
+			:icon="icon"
+			size="4x"
+		/>
+		{{ $t(i18n) }}
+	</div>
+</template>
+<script>
+import { useRouter } from 'vue-router';
+export default {
+	name: 'WaMenuBlock',
+	props: {
+		icon: {
+			type: Object,
+			required: true,
+		},
+		i18n: {
+			type: String,
+			required: true,
+		},
+		targetPath: {
+			type: String,
+			required: true,
+		},
+	},
+	setup(props) {
+		const router = useRouter();
+		const goToTarget = () => router.push(props.targetPath);
+		return { goToTarget };
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'src/style/utils';
+
+.is-menu-card-size{
+	width: 8em;
+	height: 8em;
+}
+</style>
--- src/test-vue-parent.vue
+++ src/test-vue-parent.vue
@@ -3,13 +3,22 @@
 		<div class="m-6">
 			<wa-plant-card :plant-info="testCard" />
 		</div>
+		<div class="m-6">
+			<wa-menu-block
+				:icon="testMenuBlock.icon"
+				:i18n="testMenuBlock.i18n"
+				:target-path="testMenuBlock.targetPath"
+			/>
+		</div>
 	</div>
 </template>
 
 <script>
 import WaPlantCard from 'src/components/wa-plant-card';
+import WaMenuBlock from 'src/components/wa-menu-block';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 export default {
-	components: { WaPlantCard },
+	components: { WaMenuBlock, WaPlantCard },
 	setup() {
 		const testCard = {
 			imageURL: 'https://citygreen.hu/wp-content/uploads/2017/01/Sansevieria-trifasciata.jpg',
@@ -19,7 +28,12 @@ export default {
 			isThirsty: false,
 			isStarving: false,
 		};
-		return { testCard };
+		const testMenuBlock = {
+			icon: faCog,
+			i18n: 'menu.test',
+			targetPath: '/',
+		};
+		return { testCard, testMenuBlock };
 	},
 };
 </script>
